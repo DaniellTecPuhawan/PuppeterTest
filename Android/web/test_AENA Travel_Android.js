80948a80e0bf369a138a29fd5d37c5608d257215
DaniellTecPuhawan <carlosdanielltecpuhawan@gmail.com>
2025-02-13 17:54:51 +0100

    test android web

--- Android/web/test_AENA Travel_Android.js
+++ Android/web/test_AENA Travel_Android.js
@@ -10,8 +10,6 @@ const puppeteer = require('puppeteer');
   // Navega a la página de Google
   await page.goto('https://usuarios.aena.es/?gig_ssoToken=eu1_tk1.7aWGPJKGdFLZA9_XEda5fu1mnbpTKpN7p9mK8i2DXIU&gig_originSite=travel');
 
-  
-
   // Espera a que aparezca el banner de cookies y haz clic en el botón de aceptación
   try {
     await page.waitForSelector('button#cookie-accept', { timeout: 5000 }); // Selector del botón de "Aceptar" (ajusta el selector según sea necesario)
@@ -23,16 +21,29 @@ const puppeteer = require('puppeteer');
 
   // Realizar el login (ajustando los selectores y pasos de interacción con los campos de login)
   await page.waitForSelector('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-text');
-  await page.type('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-text', 'daniell.tec@entelgy.com');
+  await page.type('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-text', 'daniell.tec@entelgy.com', { timeout: 5000 });
   console.log('✉️ Correo ingresado');
 
   await page.waitForSelector('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-password');
-  await page.type('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-password', 'Arbust0@EN@1');
+  await page.type('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-password', 'Arbust0@EN@1', { timeout: 5000 });
   console.log('🔑 Contraseña ingresada');
 
-  await page.waitForSelector('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-submit');
-  await page.click('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-submit');
-  console.log('✅ Enviado formulario de login');
+  // Espera a que el botón de submit esté disponible y haz clic
+  try {
+    await page.waitForSelector('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-submit', { timeout: 5000 });
+    const submitButton = await page.$('#gigya-login-form > .gigya-layout-row > .gigya-layout-cell > .gigya-composite-control > .gigya-input-submit');
+
+    // Verificar si el botón está habilitado
+    const isDisabled = await submitButton.evaluate(button => button.disabled);
+    if (!isDisabled) {
+      await submitButton.click();
+      console.log('✅ Formulario de login enviado');
+    } else {
+      console.log('⚠️ El botón de submit está deshabilitado.');
+    }
+  } catch (e) {
+    console.log('⚠️ No se encontró el botón de submit.');
+  }
 
   // Espera indefinidamente para que el navegador se quede abierto
   await new Promise(() => {}); 
